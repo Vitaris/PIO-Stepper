d3bd1c73286b5ebd7eeaeb40625d3c3c2af62475
Vitaris <53018032+Vitaris@users.noreply.github.com>
2025-02-23 22:31:33 +0100

    Add button handling for F1 to F4 with LCD display updates

--- pio_stepper.c
+++ pio_stepper.c
@@ -20,11 +20,27 @@ struct repeating_timer LCD_refresh_timer;
 // LCD
 lcd_t lcd;
 
+// Buttons
+button_t F1;
+button_t F2;
+button_t F3;
+button_t F4;
+
+bool F1_pressed;
+bool F2_pressed;
+bool F3_pressed;
+bool F4_pressed;
+bool some_released;
+
 bool blink_500ms;
 bool lcd_refresh;
 
 void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
 void update_blink(PIO pio, uint sm, uint freq);
+uint offset;
+
+bool stepper_enable = false;
+uint speed;
 
 void core1_entry() {
 
@@ -43,6 +59,28 @@ void core1_entry() {
             float2LCD(lcd, 4, 0, 8, (float)enc / 8000.0);
             float2LCD(lcd, 4, 1, 8, (float)(enc - enc_old) * 10 / 8000.0);
 
+            if (F1_pressed){
+                string2LCD(lcd, 0, 1, "F1");
+            } 
+            else if (F2_pressed){
+                string2LCD(lcd, 0, 1, "F2");
+            }
+            else if (F3_pressed){
+                string2LCD(lcd, 0, 1, "F3");
+            }
+            else if (F4_pressed){
+                string2LCD(lcd, 0, 1, "F4");
+            } 
+            else if (some_released) {
+                string2LCD(lcd, 0, 1, "  ");
+            }
+
+            F1_pressed = false;
+            F2_pressed = false;
+            F3_pressed = false;
+            F4_pressed = false;
+            some_released = false;
+
             enc_old = enc;
             lcd_refresh = false;
         }
@@ -50,7 +88,44 @@ void core1_entry() {
 }
 
 bool servo_timer_callback(struct repeating_timer *t) {
-    // machine_compute(cutter);
+    button_compute(F1);
+    button_compute(F2);
+    button_compute(F3); 
+    button_compute(F4);
+
+    if (F1->state_dropped){
+        F1_pressed = true;
+    } 
+    else if (F2->state_dropped){
+        pio_sm_set_enabled(pio0, 1, false);
+        pio_sm_restart(pio0, 1);
+        F2_pressed = true;
+    }
+    else if (F3->state_dropped){
+        pio_sm_set_enabled(pio0, 1, true);
+        blink_pin_forever(pio0, 1, offset, 0, speed);
+        speed += 10000;
+        F3_pressed = true;
+    }
+    else if (F4->state_dropped){
+        if (stepper_enable){
+            stepper_enable = false;
+        } 
+        else {
+            stepper_enable = true;
+        }
+        F4_pressed = true;
+    } 
+    else if (F1->state_raised || F2->state_raised || F3->state_raised || F4->state_raised) {
+        some_released = true;
+    }
+
+    if (stepper_enable) {
+        gpio_put(14, 1);
+    } else {
+        gpio_put(14, 0);
+    }
+
     return true;
 }
 
@@ -65,14 +140,22 @@ int main() {
     // Initialize
     // stdio_init_all();
 
-
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
+    offset = pio_add_program(pio, &blink_program);
     blink_pin_forever(pio, 0, offset, 25, 2);
-    blink_pin_forever(pio, 1, offset, 0, 125000);
+    speed = 10000;
+
+    F1 = create_button(10);
+    F2 = create_button(11);
+    F3 = create_button(12);
+    F4 = create_button(13);
 
     pio_add_program(pio1, &quadrature_encoder_program);
     quadrature_encoder_program_init(pio1, 0, 0, 16, 0);
+
+    stepper_enable = false;
+    gpio_init(14);
+    gpio_set_dir(14, GPIO_OUT);
 
     // Timer for servo control
     add_repeating_timer_ms(-1, servo_timer_callback, NULL, &servo_timer);
