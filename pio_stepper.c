e65cde996a53d5de4f63fca37990ca2514f86c67
Vitaris <53018032+Vitaris@users.noreply.github.com>
2025-02-27 22:03:25 +0100

    Add stepper control functionality with PIO integration

--- pio_stepper.c
+++ pio_stepper.c
@@ -8,6 +8,7 @@
 #include "hardware/clocks.h"
 #include "quadrature_encoder.pio.h"
 #include "blink.pio.h"
+#include "stepper.pio.h"
 
 #include "lcd.h"
 #include "button.h"
@@ -37,9 +38,16 @@ bool lcd_refresh;
 
 void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
 void update_blink(PIO pio, uint sm, uint freq);
-uint offset;
 
-bool stepper_enable = false;
+// Add this function declaration at the top with other declarations
+void stepper_update(PIO pio, uint sm, bool enable, bool direction, uint32_t speed);
+
+uint offset;
+uint offset_encoder;
+
+bool stepper_step;
+bool stepper_dir;
+bool stepper_enable;
 uint speed;
 
 void core1_entry() {
@@ -47,17 +55,24 @@ void core1_entry() {
     // LCD
     lcd = lcd_create(2, 3, 4, 5, 6, 7, 8, 16, 2);
     int enc_old = 0;
+    bool led_state = false;
+    bool lcd_step = false;
+    bool lcd_dir = false;
     while (1)
     {
         if (lcd_refresh == true)
         {
+            led_state = gpio_get(PICO_DEFAULT_LED_PIN);
+            lcd_step = gpio_get(0);
+            lcd_dir = gpio_get(1);
+
             int enc = quadrature_encoder_get_count(pio1, 0);
             string2LCD(lcd, 0, 0, "Enc:");
             string2LCD(lcd, 13, 0, "rev");
-            string2LCD(lcd, 13, 1, "r/s");
+            // string2LCD(lcd, 13, 1, "r/s");
             // int2LCD(lcd, 0, 1, 6, a++); 
             float2LCD(lcd, 4, 0, 8, (float)enc / 8000.0);
-            float2LCD(lcd, 4, 1, 8, (float)(enc - enc_old) * 10 / 8000.0);
+            // float2LCD(lcd, 4, 1, 8, (float)(enc - enc_old) * 10 / 8000.0);
 
             if (F1_pressed){
                 string2LCD(lcd, 0, 1, "F1");
@@ -73,6 +88,31 @@ void core1_entry() {
             } 
             else if (some_released) {
                 string2LCD(lcd, 0, 1, "  ");
+            }
+            if (false) {
+                string2LCD(lcd, 0, 1, "Stp:");
+                if (led_state){
+                    string2LCD(lcd, 4, 1, "1");
+                } 
+                else {
+                    string2LCD(lcd, 4, 1, "0");
+                }
+            }
+
+            string2LCD(lcd, 0, 1, "Stp:");
+            if (lcd_step){
+                string2LCD(lcd, 4, 1, "1");
+            } 
+            else {
+                string2LCD(lcd, 4, 1, "0");
+            }
+
+            string2LCD(lcd, 7, 1, "Dir:");
+            if (lcd_dir){
+                string2LCD(lcd, 11, 1, "1");
+            } 
+            else {
+                string2LCD(lcd, 11, 1, "0");
             }
 
             F1_pressed = false;
@@ -135,14 +175,12 @@ bool LCD_refresh_timer_callback(struct repeating_timer *t) {
 }
 
 int main() {
-    // cutter = create_machine();
-
-    // Initialize
-    // stdio_init_all();
-
     PIO pio = pio0;
-    offset = pio_add_program(pio, &blink_program);
-    blink_pin_forever(pio, 0, offset, 25, 2);
+    // offset = pio_add_program(pio, &blink_program);
+    offset = pio_add_program(pio, &stepper_program);
+    // blink_pin_forever(pio, 0, offset, 25, 1);
+    stepper_program_init(pio, 0, offset, 0, 1);
+    stepper_update(pio, 0, 1, 0, 1);
     speed = 10000;
 
     F1 = create_button(10);
@@ -150,9 +188,11 @@ int main() {
     F3 = create_button(12);
     F4 = create_button(13);
 
-    pio_add_program(pio1, &quadrature_encoder_program);
-    quadrature_encoder_program_init(pio1, 0, 0, 16, 0);
+    offset_encoder = pio_add_program(pio1, &quadrature_encoder_program);
+    quadrature_encoder_program_init(pio1, 0, offset_encoder, 16, 0);
 
+    stepper_step = false;
+    stepper_dir = false;
     stepper_enable = false;
     gpio_init(14);
     gpio_set_dir(14, GPIO_OUT);
@@ -188,4 +228,17 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
 
 void update_blink(PIO pio, uint sm, uint freq) {
     pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
+}
+
+// Add this function implementation
+void stepper_update(PIO pio, uint sm, bool enable, bool direction, uint32_t freq) {
+    // Create 32-bit word where:
+    // bit 0 = enable
+    // bit 1 = direction
+    // bits 2-31 = speed
+    uint32_t speed = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
+    uint32_t data = (speed << 2) | (direction << 1) | enable;
+    
+    // Send to PIO state machine
+    pio_sm_put_blocking(pio, sm, data);
 }
